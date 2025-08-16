6e03ecc0337a00bc2ebc51734437998bd0c290de
LAPrice90 <laprice90@gmail.com>
2025-08-16 13:20:20 +0100

    Add Firebase config and hook up Google login

---
+++ js/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAChauzqXrxR96_YMt0SJs2ygk9FQ6FpwU",
+  authDomain: "flashcards-app-2f89e.firebaseapp.com",
+  projectId: "flashcards-app-2f89e",
+  storageBucket: "flashcards-app-2f89e.firebasestorage.app",
+  messagingSenderId: "886681573920",
+  appId: "1:886681573920:web:65a3133bbbadfa645dfa45",
+  measurementId: "G-3JK7388FG1"
+};
+
+export default firebaseConfig;
